d8655f835e7c02b4b9de19786b2316b6ac83e2e8
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-27 01:59:30 +0200

    Implement IGoldPriceRepository

--- PreciousDotNet/Api/GoldPrice/Repository/GoldPriceRepository.cs
+++ PreciousDotNet/Api/GoldPrice/Repository/GoldPriceRepository.cs
@@ -1,6 +1,24 @@
-﻿namespace PreciousDotNet.Api
+﻿using PreciousDotNet.Api.GoldPrice.Service;
+using PreciousDotNet.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PreciousDotNet.Api
 {
-    public class GoldPriceRepository
+    public class GoldPriceRepository : IGoldPriceRepository
     {
+        private readonly IGoldPriceService _goldPriceService = new GoldPriceService();
+
+        public Task<HttpResponseDto> GetGoldPriceAsync(string currency = "EUR") => _goldPriceService.GetGoldPriceAsync(currency);
+
+        public Task<HttpResponseDto> GetGoldPriceAsync(IEnumerable<string> currencies) => _goldPriceService.GetGoldPriceAsync(currencies);
+
+        public Task<IEnumerable<string>> GetCurrenciesAsync() => _goldPriceService.GetCurrenciesAsync();
+
+        public HttpResponseDto GetGoldPrice(string currency = "EUR") => _goldPriceService.GetGoldPrice(currency);
+
+        public HttpResponseDto GetGoldPrice(IEnumerable<string> currencies) => _goldPriceService.GetGoldPrice(currencies);
+
+        public IEnumerable<string> GetCurrencies() => _goldPriceService.GetCurrencies();
     }
 }
