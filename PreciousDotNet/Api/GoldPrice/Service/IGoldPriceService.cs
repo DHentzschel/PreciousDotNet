e2195340091d81affed7ec7fccb49694f0b42f7a
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-27 01:58:22 +0200

    Implement predefined function declarations

--- PreciousDotNet/Api/GoldPrice/Service/IGoldPriceService.cs
+++ PreciousDotNet/Api/GoldPrice/Service/IGoldPriceService.cs
@@ -1,6 +1,21 @@
+using PreciousDotNet.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace PreciousDotNet.Api.GoldPrice.Service
 {
     interface IGoldPriceService
     {
+        Task<HttpResponseDto> GetGoldPriceAsync(string currency);
+
+        Task<HttpResponseDto> GetGoldPriceAsync(IEnumerable<string> currencies);
+
+        Task<IEnumerable<string>> GetCurrenciesAsync();
+
+        HttpResponseDto GetGoldPrice(string currency);
+
+        HttpResponseDto GetGoldPrice(IEnumerable<string> currencies);
+
+        IEnumerable<string> GetCurrencies();
     }
 }
