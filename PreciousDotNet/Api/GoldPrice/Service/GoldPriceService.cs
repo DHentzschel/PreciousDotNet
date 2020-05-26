055412a6c83bf2b1ea442d9e1d9f05c67488a845
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-27 01:58:46 +0200

    Implement NYI GoldPriceService

--- PreciousDotNet/Api/GoldPrice/Service/GoldPriceService.cs
+++ PreciousDotNet/Api/GoldPrice/Service/GoldPriceService.cs
@@ -1,7 +1,40 @@
 
+using PreciousDotNet.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace PreciousDotNet.Api.GoldPrice.Service
 {
-    class GoldPriceService
+    class GoldPriceService : IGoldPriceService
     {
+        public IEnumerable<string> GetCurrencies()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<string>> GetCurrenciesAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public HttpResponseDto GetGoldPrice(string currency = "EUR")
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public HttpResponseDto GetGoldPrice(IEnumerable<string> currencies)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<HttpResponseDto> GetGoldPriceAsync(string currency = "EUR")
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<HttpResponseDto> GetGoldPriceAsync(IEnumerable<string> currencies)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
