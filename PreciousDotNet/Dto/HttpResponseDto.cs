3ad2b37fa7a1b0774264fd5f97f2d4d3d3ba00f7
Daniel Hentzschel <5008270+DHentzschel@users.noreply.github.com>
2020-05-27 01:29:46 +0200

    Implement HttpResponseDto

---
+++ PreciousDotNet/Dto/HttpResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace PreciousDotNet.Dto
+{
+    public class HttpResponseDto
+    {
+        public ulong Ts { get; set; }
+
+        public ulong Tsj { get; set; }
+
+        public List<CurrencyDto> Items { get; set; }
+    }
+}
